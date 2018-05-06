e320893ca272b2de50e8dc22bf150b2e8d9cea50
heronzo <odhiamboherons12@gmail.com>
2018-05-06 13:23:49 +0300

    Carousel added, footer, formatting

---
+++ home.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <title>Home</title>
+        <meta charset="utf-8">
+	<!--make it resposive to screen size using bootstrap-->
+	<meta name="viewport" content="width=device-width, initial-scaled=1.0">
+	<!--title> School Information Management System</title-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+    </head>
+    <body>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="Images/nissan.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>LA is always so much fun!</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="Images/ix.jpg" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="Images/imaes.jpg" alt="New York">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    </body>
+</html>
